cdc3ff5829644c51c8d796fb45e2de40510dab33
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-08-15 13:42:09 -0300

    test: ensure AxiosHttpClient rethrows if axios throws

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -44,5 +44,13 @@ describe('AxiosHttpClient', () => {
 
       expect(result).toEqual('any_data')
     })
+
+    it('should rethrow if get throws', async () => {
+      fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+
+      const result = sut.get({ url, params })
+
+      await expect(result).rejects.toThrow(new Error('http_error'))
+    })
   })
 })
