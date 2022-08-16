6ffb38df2bfeadb6fbc83c6f8c7f03194e949836
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-08-16 14:31:54 -0300

    refactor: improve code quality

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -4,9 +4,10 @@ import { PgUser } from '@/infra/postgres/entities'
 import { getRepository } from 'typeorm'
 
 export class PgUserAccountRepository implements LoadUserAccountRepository {
+  private readonly pgUserRepo = getRepository(PgUser)
+
   async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
-    const pgUserRepo = getRepository(PgUser)
-    const pgUser = await pgUserRepo.findOne({ email: params.email })
+    const pgUser = await this.pgUserRepo.findOne({ email: params.email })
     if (pgUser !== undefined) {
       return {
         id: pgUser.id.toString(),
@@ -16,15 +17,14 @@ export class PgUserAccountRepository implements LoadUserAccountRepository {
   }
 
   async saveWithFacebook (params: SaveFacebookAccountRepository.Params): Promise<void> {
-    const pgUserRepo = getRepository(PgUser)
     if (params.id === undefined) {
-      await pgUserRepo.save({
+      await this.pgUserRepo.save({
         name: params.name,
         email: params.email,
         facebookId: params.facebookId
       })
     } else {
-      await pgUserRepo.update({
+      await this.pgUserRepo.update({
         id: parseInt(params.id)
       }, {
         name: params.name,
