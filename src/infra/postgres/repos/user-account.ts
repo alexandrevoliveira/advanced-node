e44cbfdcc0aa7b08030b07966ccc3fccc4296258
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-08-16 17:36:24 -0300

    feat: make PgUserAccountRepository implement SaveFacebookAccountRepository interface

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -3,7 +3,7 @@ import { PgUser } from '@/infra/postgres/entities'
 
 import { getRepository } from 'typeorm'
 
-export class PgUserAccountRepository implements LoadUserAccountRepository {
+export class PgUserAccountRepository implements LoadUserAccountRepository, SaveFacebookAccountRepository {
   private readonly pgUserRepo = getRepository(PgUser)
 
   async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
