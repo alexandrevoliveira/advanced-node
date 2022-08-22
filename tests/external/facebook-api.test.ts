a72cafb6c4c36c7d3c6be1ad61f21770ef4da14e
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-08-22 16:20:56 -0300

    test: ensure FacebookApi returns undefined when HttpGetClient throws

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,35 @@
-describe('', () => {
-  it('', () => {
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
 
+describe('Facebook Api Integration Tests', () => {
+  it('should return a Facebook User if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'EAAFN6IfFATUBAPFVkUShZAVOZA3baXLRfcbiJ6jev7o8YuTIZAQH4B70WbLNMrZA6ZCy5aoSmLpttZAgEGeH5mEZAEUPLGhp78cWvwMsOZAL3wTdWwGe6OTF9ZCQWo7xkQsZBNacZCmk9at6VF2h2hWLVTn9WII6XlZBE6q4Ugta2hHrpfajmVrispUZCJNV4nu2LiqeqqUAAJgaITwZDZD' })
+
+    expect(fbUser).toEqual({
+      facebookId: '103885812449051',
+      name: 'Alexandre Teste',
+      email: 'alexandre_nwqpczg_teste@tfbnw.net'
+    })
+  })
+
+  it('should return undefined if token is invalid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'invalid' })
+
+    expect(fbUser).toBeUndefined()
   })
 })
