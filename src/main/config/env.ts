5ab1b740eba1b742609b018f83dff976ce24a0fa
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-08-22 19:09:00 -0300

    feat: configure express server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '367136082297141',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '420bce2efe0dfebd033ac0509132d426'
-  }
+  },
+  port: process.env.PORT ?? 8080
 }
