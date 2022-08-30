b55eaaafdbe79d049260d5f30c91721d0c6d005a
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-08-29 22:02:13 -0300

    refactor: rename JwtTokenGenerator to JwtTokenHandler

--- src/main/factories/usecases/facebook-authentication.ts
+++ src/main/factories/usecases/facebook-authentication.ts
@@ -1,12 +1,12 @@
 import { FacebookAuthentication, setupFacebookAuthentication } from '@/domain/usecases'
 import { makeFacebookApi } from '@/main/factories/apis'
-import { makeJwtTokenGenerator } from '@/main/factories/crypto'
+import { makeJwtTokenHandler } from '@/main/factories/crypto'
 import { makePgUserAccountRepo } from '@/main/factories/repos'
 
 export const makeFacebookAuthentication = (): FacebookAuthentication => {
   return setupFacebookAuthentication(
     makeFacebookApi(),
     makePgUserAccountRepo(),
-    makeJwtTokenGenerator()
+    makeJwtTokenHandler()
   )
 }
