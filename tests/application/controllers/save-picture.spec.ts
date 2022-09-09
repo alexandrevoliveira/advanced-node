529bd68dc5dcaa5ea48a2f98d213db3640c8ef99
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-09-09 09:43:02 -0300

    refactor: improve error text

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -14,7 +14,7 @@ class SavePictureController {
 
 class InvalidMimeTypeError extends Error {
   constructor (allowed: string[]) {
-    super(`Unsupported type. Allowed types: ${allowed.join(', ')}`)
+    super(`Unsupported file. Allowed extensions: ${allowed.join(', ')}`)
     this.name = 'InvalidMimeTypeError'
   }
 }
