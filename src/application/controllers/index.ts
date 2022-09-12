f313279705bcf23b892dd09d801ea850f28a9fb2
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-09-12 12:43:52 -0300

    feat: merge delete and save picture controllers

--- src/application/controllers/index.ts
+++ src/application/controllers/index.ts
@@ -1,4 +1,3 @@
 export * from './controller'
-export * from './delete-picture'
 export * from './facebook-login'
 export * from './save-picture'
--- src/main/factories/application/controllers/index.ts
+++ src/main/factories/application/controllers/index.ts
@@ -1,2 +1,2 @@
-export * from './delete-picture'
 export * from './facebook-login'
+export * from './save-picture'
