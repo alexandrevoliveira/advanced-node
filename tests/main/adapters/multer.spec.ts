ba80be687f5012faf8c67b9176845238837a02ac
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-09-12 18:57:54 -0300

    refactor: move MulterAdapter to its own file

--- tests/main/adapters/multer.spec.ts
+++ tests/main/adapters/multer.spec.ts
@@ -1,23 +1,11 @@
 import { ServerError } from '@/application/errors'
+import { adaptMulter } from '@/main/adapters'
 
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { NextFunction, Request, RequestHandler, Response } from 'express'
 import multer from 'multer'
 
 jest.mock('multer')
-
-const adaptMulter: RequestHandler = (req, res, next) => {
-  const upload = multer().single('picture')
-  upload(req, res, error => {
-    if (error !== undefined) {
-      return res.status(500).json({ error: new ServerError(error).message })
-    }
-    if (req.file !== undefined) {
-      req.locals = { ...req.locals, file: { buffer: req.file.buffer, mimeType: req.file.mimetype } }
-    }
-    next()
-  })
-}
 
 describe('MulterAdapter', () => {
   let uploadSpy: jest.Mock
