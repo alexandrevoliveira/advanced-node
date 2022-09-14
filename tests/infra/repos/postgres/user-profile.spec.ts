2ab543cacb148c37ff9920a67e172004981e7a6a
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-09-14 00:39:09 -0300

    refactor: apply PgConnection

--- tests/infra/repos/postgres/user-profile.spec.ts
+++ tests/infra/repos/postgres/user-profile.spec.ts
@@ -1,28 +1,35 @@
-import { PgUserProfileRepository } from '@/infra/repos/postgres'
+import { PgRepository, PgUserProfileRepository } from '@/infra/repos/postgres'
 import { PgUser } from '@/infra/repos/postgres/entities'
+import { PgConnection } from '@/infra/repos/postgres/helpers'
 import { makeFakeDb } from '@/tests/infra/repos/postgres/mocks'
 
 import { IBackup } from 'pg-mem'
-import { getRepository, Repository, getConnection } from 'typeorm'
+import { Repository } from 'typeorm'
 
 describe('PgUserProfileRepository', () => {
   let sut: PgUserProfileRepository
+  let connection: PgConnection
   let pgUserRepo: Repository<PgUser>
   let backup: IBackup
 
   beforeAll(async () => {
+    connection = PgConnection.getInstance()
     const db = await makeFakeDb([PgUser])
     backup = db.backup()
-    pgUserRepo = getRepository(PgUser)
+    pgUserRepo = connection.getRepository(PgUser)
   })
 
   afterAll(async () => {
-    await getConnection().close()
+    await connection.disconnect()
   })
 
   beforeEach(() => {
     backup.restore()
     sut = new PgUserProfileRepository()
+  })
+
+  it('should extend PgRepository', async () => {
+    expect(sut).toBeInstanceOf(PgRepository)
   })
 
   describe('savePicture', () => {
