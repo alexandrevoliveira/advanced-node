83a3d61abd81ecd9c02d69d8112ad95935247afd
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2022-09-13 22:51:18 -0300

    test: ensure PgConnection open transaction

--- tests/infra/repos/postgres/helpers/connection.spec.ts
+++ tests/infra/repos/postgres/helpers/connection.spec.ts
@@ -18,6 +18,7 @@ describe('PgConnection', () => {
   let getConnectionSpy: jest.Mock
   let hasSpy: jest.Mock
   let closeSpy: jest.Mock
+  let startTransactionSpy: jest.Mock
   let sut: PgConnection
 
   beforeAll(() => {
@@ -26,7 +27,10 @@ describe('PgConnection', () => {
       has: hasSpy
     })
     jest.mocked(getConnectionManager).mockImplementation(getConnectionManagerSpy)
-    createQueryRunnerSpy = jest.fn().mockReturnValue({})
+    startTransactionSpy = jest.fn()
+    createQueryRunnerSpy = jest.fn().mockReturnValue({
+      startTransaction: startTransactionSpy
+    })
     createConnectionSpy = jest.fn().mockResolvedValue({
       createQueryRunner: createQueryRunnerSpy
     })
@@ -81,6 +85,23 @@ describe('PgConnection', () => {
     const promise = sut.disconnect()
 
     expect(closeSpy).not.toHaveBeenCalled()
+    await expect(promise).rejects.toThrow(new ConnectionNotFoundError())
+  })
+
+  it('should open transaction', async () => {
+    await sut.connect()
+    await sut.openTransaction()
+
+    expect(startTransactionSpy).toHaveBeenCalledWith()
+    expect(startTransactionSpy).toHaveBeenCalledTimes(1)
+
+    await sut.disconnect()
+  })
+
+  it('should return ConnectionNotFoundError on openTransaction if connection is not found', async () => {
+    const promise = sut.openTransaction()
+
+    expect(startTransactionSpy).not.toHaveBeenCalled()
     await expect(promise).rejects.toThrow(new ConnectionNotFoundError())
   })
 })
